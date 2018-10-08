eb505c01aa2a3f44a341996266bbec30739e29a7
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-08 15:17:24 -0700

    cars

--- CARS/create.sql
+++ CARS/create.sql
@@ -1,26 +1,46 @@
 CREATE TABLE CarNames 
 (
-CarID int,
-Model varchar(255),
-Make varchar(255)
+    CarID int,
+    Model varchar(255),
+    Make varchar(255)
 );
 
-CREATE TABLE CarNames 
+CREATE TABLE CarMakers 
 (
-MakerID int,
-Maker varchar(255),
-FullName varchar(255),
-Country varchar(255)
+    MakerID int,
+    Maker varchar(255),
+    FullName varchar(255),
+    Country varchar(255)
 );
 
 CREATE TABLE CarsData
 (
-ID int,
-MPG int,
-Cylinders int
-Edispl int,
-Horsepower int,
-Weight int,
-Accelerate float,
-Year int
+    ID int,
+    MPG int,
+    Cylinders int
+    Edispl int,
+    Horsepower int,
+    Weight int,
+    Accelerate float,
+    Year int
 );
+
+CREATE TABLE Continent
+(
+    ContID int,
+    Continent varchar(255)
+)
+
+CREATE TABLE Country
+(
+    CountryId int,
+    CountryName varchar(255),
+    Continent varchar(255)
+)
+
+CREATE TABLE ModelList
+(
+    ModelId int,
+    Maker varchar(255),
+    Model varchar(255)
+)
