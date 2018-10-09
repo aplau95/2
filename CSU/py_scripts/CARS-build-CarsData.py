825f852e4e7df1ac3cfbc17eb70cfb21b9abfd48
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-08 23:55:15 -0700

    whoops

---
+++ CSU/py_scripts/CARS-build-CarsData.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import csv
+
+connection = mysql.connector.connect(
+    host="ambari-head.csc.calpoly.edu",
+    user="alau23",
+    passwd="6184431",
+    database="alau23"
+)
+
+with open ('../csv_files/car-data.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO CarData VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
