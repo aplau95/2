3385d19a5284cb22230900c75eef07ed41b57b08
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-08 23:25:05 -0700

    fix delete tables

--- CARS/deleteTables.sql
+++ CARS/deleteTables.sql
@@ -1,15 +1,7 @@
+DROP TABLE CarNames; 
+DROP TABLE CarMakers; 
+DROP TABLE CarsData;
+DROP TABLE Continent;
+DROP TABLE Country;
+DROP TABLE ModelList;
 
-
-CREATE TABLE CarMakers 
-(
-    MakerID int,
-    Maker varchar(255),
-    FullName varchar(255),
-    Country varchar(255)
-);
-
-
-LOAD DATA INFILE 'car-makers.csv' INTO TABLE CarMakers
-FIELDS TERMINATED BY '\n' 
-ENCLOSED BY ','
-IGNORE 1 LINES; 
