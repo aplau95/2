5905f3acfb4483f79bac78a511f226f233637c94
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-08 23:14:46 -0700

    cleaning up

--- CARS/populateTables.py
+++ CARS/populateTables.py
@@ -8,7 +8,7 @@ connection = mysql.connector.connect(
     database="alau23"
 )
 
-with open ('test.csv', 'r') as f:
+with open ('car-makers.csv', 'r') as f:
     reader = csv.reader(f)
     data = next(reader) 
     data = next(reader)
@@ -19,3 +19,57 @@ with open ('test.csv', 'r') as f:
         cursor.execute(query % tuple(data))
     connection.commit()
     
+with open ('car-names.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO CarNames VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
+
+with open ('car-data.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO CarData VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
+
+with open ('countries.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO Country VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
+
+with open ('model-list.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO ModelList VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
+
+with open ('continents.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = next(reader) 
+    data = next(reader)
+    query = 'INSERT INTO Continent VALUES({0})'
+    query = query.format(', '.join(['%s'] * len(data)))
+    cursor = connection.cursor()
+    for data in reader:
+        cursor.execute(query % tuple(data))
+    connection.commit()
