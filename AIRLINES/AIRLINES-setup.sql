9b8ab9ce78c6f6827667917b566563d7379c8dd2
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-09 21:07:25 -0700

    fixing arlines sql

--- AIRLINES/AIRLINES-setup.sql
+++ AIRLINES/AIRLINES-setup.sql
@@ -1,27 +1,27 @@
 CREATE TABLE Airlines(
-Id INT,
-Airline VARCHAR(255),
-Abbreviation VARCHAR(255),
-Country VARCHAR(255),
-PRIMARY KEY (Id)
+    Id INT,
+    Airline VARCHAR(255),
+    Abbreviation VARCHAR(255),
+    Country VARCHAR(255),
+    PRIMARY KEY (Id)
 );
 
 CREATE TABLE Airports(
-City VARCHAR(255),
-AirportCode CHAR(3),
-AirportName VARCHAR(255),
-Country VARCHAR(255),
-Abbrev VARCHAR(255)
-PRIMARY KEY (AirportCode)
+    City VARCHAR(255),
+    AirportCode VARCHAR(3),
+    AirportName VARCHAR(255),
+    Country VARCHAR(255),
+    Abbrev VARCHAR(255),
+    PRIMARY KEY (AirportCode)
 );
 
 CREATE TABLE Flights(
-Airline INT,
-FlightNo INT,
-SourceAirport CHAR(3),
-DestAirport CHAR(3)
-PRIMARY KEY (Airline, FightNo),
-FOREIGN KEY (SourceAirport) REFERENCES Airports(AirportCode),
-FOREIGN KEY (DestAirport) REFERENCES Airports(AirportCode)
+    Airline INT,
+    FlightNo INT,
+    SourceAirport VARCHAR(3),
+    DestAirport VARCHAR(3)
+    PRIMARY KEY (Airline, FightNo),
+    FOREIGN KEY (SourceAirport) REFERENCES Airports(AirportCode),
+    FOREIGN KEY (DestAirport) REFERENCES Airports(AirportCode)
 );
 
