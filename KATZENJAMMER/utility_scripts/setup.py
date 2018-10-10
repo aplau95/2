185604557808a911946603c07f6339af9511fe61
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-09 19:16:41 -0700

    katzenshit

---
+++ KATZENJAMMER/utility_scripts/setup.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import csv
+
+connection = mysql.connector.connect(
+    host="ambari-head.csc.calpoly.edu",
+    user="alau23",
+    passwd="6184431",
+    database="alau23"
+)
+
+cursor = connection.cursor()
+
+def executeScript(filename):
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+    for line in sqlCommands:
+        try:
+            if line.strip() != '':
+                cursor.execute(line)
+        except IOError, msg:
+            print "Command skipped: ", msg
+
+executeScript('../KATZENJAMMER-setup.sql')
+connection.commit()
