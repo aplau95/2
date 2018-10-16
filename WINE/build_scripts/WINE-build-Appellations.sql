280f13afac9fbc9bfc9d5cefb8cf1b72840b19a6
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-16 11:51:29 -0700

    asdf

--- WINE/build_scripts/WINE-build-Appellations.sql
+++ WINE/build_scripts/WINE-build-Appellations.sql
@@ -1,57 +1,57 @@
-INSERT INTO Appellations VALUES(1, 'Alexander Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(2, 'Amador County', 'Amador', 'Sierra Foothills', 'No')
-INSERT INTO Appellations VALUES(3, 'Amador-Mendocino-Sonoma Counties', 'N/A', 'N/A', 'No')
-INSERT INTO Appellations VALUES(4, 'Anderson Valley', 'Mendocino', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(5, 'Arroyo Grande Valley', 'San Luis Obispo', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(6, 'Atlas Peak', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(7, 'Bennett Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(8, 'Calaveras County', 'Calaveras', 'Sierra Foothills', 'No')
-INSERT INTO Appellations VALUES(9, 'California', 'N/A', 'California', 'No')
-INSERT INTO Appellations VALUES(10, 'Carneros', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(11, 'Central Coast', 'N/A', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(12, 'Chalk Hill', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(13, 'Chalone', 'Monterey', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(14, 'Contra Costa County', 'Contra Costa', 'Santa Cruz Mountains', 'No')
-INSERT INTO Appellations VALUES(15, 'Dry Creek Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(16, 'Edna Valley', 'San Luis Obispo', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(17, 'Fiddletown', 'Amador', 'Sierra Foothills', 'Yes')
-INSERT INTO Appellations VALUES(18, 'Green Valley of Russian River Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(19, 'Guenoc Valley', 'Lake', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(20, 'Happy Canyon of Santa Barbara', 'Santa Barbara', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(21, 'Howell Mountain', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(22, 'Knights Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(23, 'Lake County', 'Lake', 'North Coast', 'No')
-INSERT INTO Appellations VALUES(24, 'Livermore Valley', 'Alameda', 'Santa Cruz Mountains', 'Yes')
-INSERT INTO Appellations VALUES(25, 'Lodi', 'San Joaquin', 'Central Valley', 'Yes')
-INSERT INTO Appellations VALUES(26, 'Mendocino County', 'Mendocino', 'North Coast', 'No')
-INSERT INTO Appellations VALUES(27, 'Monterey County', 'Monterey', 'Central Coast', 'No')
-INSERT INTO Appellations VALUES(28, 'Mount Harlan', 'San Benito', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(29, 'Mount Vedeer', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(30, 'Napa Valley', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(31, 'Napa-Sonoma counties', 'N/A', 'North Coast', 'No')
-INSERT INTO Appellations VALUES(32, 'North Coast', 'N/A', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(33, 'Oakville', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(34, 'Paso Robles', 'San Luis Obispo', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(35, 'Red Hills Lake County', 'Lake', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(36, 'Rockpile', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(37, 'Russian River Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(38, 'Rutherford', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(39, 'San Luis Obispo County', 'San Luis Obispo', 'Central Coast', 'No')
-INSERT INTO Appellations VALUES(40, 'Santa Barbara County', 'Santa Barbara', 'Central Coast', 'No')
-INSERT INTO Appellations VALUES(41, 'Santa Cruz Mountains', 'N/A', 'Santa Cruz Mountains', 'Yes')
-INSERT INTO Appellations VALUES(42, 'Santa Lucia Highlands', 'Monterey', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(43, 'Santa Maria Valley', 'Santa Barbara', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(44, 'Santa Ynez Valley', 'Santa Barbara', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(45, 'Shenandoah Valley', 'Amador', 'Sierra Foothills', 'Yes')
-INSERT INTO Appellations VALUES(46, 'Sierra Foothills', 'N/A', 'Sierra Foothills', 'Yes')
-INSERT INTO Appellations VALUES(47, 'Solano County', 'Solano', 'North Coast', 'No')
-INSERT INTO Appellations VALUES(48, 'Sonoma Coast', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(49, 'Sonoma County', 'Sonoma', 'North Coast', 'No')
-INSERT INTO Appellations VALUES(50, 'Sonoma Valley', 'Sonoma', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(51, 'Sonoma-Lake Counties', 'N/A', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(52, 'Spring Mountain District', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(53, 'St. Helena', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(54, 'Sta. Rita Hills', 'Santa Barbara', 'Central Coast', 'Yes')
-INSERT INTO Appellations VALUES(55, 'Stags Leap District', 'Napa', 'North Coast', 'Yes')
-INSERT INTO Appellations VALUES(56, 'Yolo County', 'Yolo', 'Central Valley', 'No')
-INSERT INTO Appellations VALUES(57, 'Yountville', 'Napa', 'North Coast', 'Yes')
+INSERT INTO Appellations VALUES(1, 'Alexander Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(2, 'Amador County', 'Amador', 'Sierra Foothills', 'No');
+INSERT INTO Appellations VALUES(3, 'Amador-Mendocino-Sonoma Counties', 'N/A', 'N/A', 'No');
+INSERT INTO Appellations VALUES(4, 'Anderson Valley', 'Mendocino', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(5, 'Arroyo Grande Valley', 'San Luis Obispo', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(6, 'Atlas Peak', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(7, 'Bennett Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(8, 'Calaveras County', 'Calaveras', 'Sierra Foothills', 'No');
+INSERT INTO Appellations VALUES(9, 'California', 'N/A', 'California', 'No');
+INSERT INTO Appellations VALUES(10, 'Carneros', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(11, 'Central Coast', 'N/A', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(12, 'Chalk Hill', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(13, 'Chalone', 'Monterey', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(14, 'Contra Costa County', 'Contra Costa', 'Santa Cruz Mountains', 'No');
+INSERT INTO Appellations VALUES(15, 'Dry Creek Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(16, 'Edna Valley', 'San Luis Obispo', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(17, 'Fiddletown', 'Amador', 'Sierra Foothills', 'Yes');
+INSERT INTO Appellations VALUES(18, 'Green Valley of Russian River Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(19, 'Guenoc Valley', 'Lake', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(20, 'Happy Canyon of Santa Barbara', 'Santa Barbara', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(21, 'Howell Mountain', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(22, 'Knights Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(23, 'Lake County', 'Lake', 'North Coast', 'No');
+INSERT INTO Appellations VALUES(24, 'Livermore Valley', 'Alameda', 'Santa Cruz Mountains', 'Yes');
+INSERT INTO Appellations VALUES(25, 'Lodi', 'San Joaquin', 'Central Valley', 'Yes');
+INSERT INTO Appellations VALUES(26, 'Mendocino County', 'Mendocino', 'North Coast', 'No');
+INSERT INTO Appellations VALUES(27, 'Monterey County', 'Monterey', 'Central Coast', 'No');
+INSERT INTO Appellations VALUES(28, 'Mount Harlan', 'San Benito', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(29, 'Mount Vedeer', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(30, 'Napa Valley', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(31, 'Napa-Sonoma counties', 'N/A', 'North Coast', 'No');
+INSERT INTO Appellations VALUES(32, 'North Coast', 'N/A', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(33, 'Oakville', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(34, 'Paso Robles', 'San Luis Obispo', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(35, 'Red Hills Lake County', 'Lake', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(36, 'Rockpile', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(37, 'Russian River Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(38, 'Rutherford', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(39, 'San Luis Obispo County', 'San Luis Obispo', 'Central Coast', 'No');
+INSERT INTO Appellations VALUES(40, 'Santa Barbara County', 'Santa Barbara', 'Central Coast', 'No');
+INSERT INTO Appellations VALUES(41, 'Santa Cruz Mountains', 'N/A', 'Santa Cruz Mountains', 'Yes');
+INSERT INTO Appellations VALUES(42, 'Santa Lucia Highlands', 'Monterey', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(43, 'Santa Maria Valley', 'Santa Barbara', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(44, 'Santa Ynez Valley', 'Santa Barbara', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(45, 'Shenandoah Valley', 'Amador', 'Sierra Foothills', 'Yes');
+INSERT INTO Appellations VALUES(46, 'Sierra Foothills', 'N/A', 'Sierra Foothills', 'Yes');
+INSERT INTO Appellations VALUES(47, 'Solano County', 'Solano', 'North Coast', 'No');
+INSERT INTO Appellations VALUES(48, 'Sonoma Coast', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(49, 'Sonoma County', 'Sonoma', 'North Coast', 'No');
+INSERT INTO Appellations VALUES(50, 'Sonoma Valley', 'Sonoma', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(51, 'Sonoma-Lake Counties', 'N/A', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(52, 'Spring Mountain District', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(53, 'St. Helena', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(54, 'Sta. Rita Hills', 'Santa Barbara', 'Central Coast', 'Yes');
+INSERT INTO Appellations VALUES(55, 'Stags Leap District', 'Napa', 'North Coast', 'Yes');
+INSERT INTO Appellations VALUES(56, 'Yolo County', 'Yolo', 'Central Valley', 'No');
+INSERT INTO Appellations VALUES(57, 'Yountville', 'Napa', 'North Coast', 'Yes');
