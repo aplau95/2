380e1303386d16ab89933a7fcd72265a95e4d45a
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-16 18:15:15 -0700

    adding STUDENTS

---
+++ STUDENTS/STUDENTS-modify.sql
@@ -0,0 +1,27 @@
+ALTER TABLE Students ADD COLUMN GPA Decimal(3,2);
+DELETE FROM Students Where Grade != 5 AND Grade != 6;
+INSERT INTO Teachers VALUES('GAWAIN', 'AP-MAWR', 120);
+
+UPDATE Students Set Classroom = 120 
+WHERE FirstName = 'JEFFRY' AND LastName = 'FLACHS' 
+OR FirstName = 'TAWANNA' AND LastName = 'HUANG'
+OR FirstName = 'ELVIRA' AND LastName = 'JAGNEAUX';
+
+UPDATE Students Set GPA = 3.25 
+WHERE Grade = 6;
+
+UPDATE Students Set GPA = 2.9 
+WHERE Grade = 5 AND Classroom = 109;
+
+UPDATE Students Set GPA = 3.5 
+WHERE Grade = 5 AND Classroom = 120;
+
+UPDATE Students Set GPA = 4.0 
+WHERE FirstName = 'CHET' AND LastName = 'MACIAG';
+
+UPDATE Students Set GPA = GPA + .3 
+WHERE FirstName = 'AL' AND LastName = 'GERSTEIN';
+
+UPDATE Students Set GPA = GPA * 1.1 
+WHERE FirstName = 'TAWANNA' AND LastName = 'HUANG'
+OR FirstName = 'ELVIRA' AND LastName = 'JAGNEAUX';
