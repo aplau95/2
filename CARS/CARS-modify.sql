48791b9ab9fd1f0e9c84384b9093d8d8e1f4872e
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-16 18:42:42 -0700

    moving files in build scripts in CARS

--- CARS/CARS-modify.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM CarsData WHERE NOT (1979 <= Year AND Year <= 1980 AND MPG >= 20) 
-AND NOT (MPG >= 26 AND MPG IS NOT NULL AND Horsepower > 110 AND Horsepower IS NOT NULL)
-AND NOT (Cylinders = 8 AND Accelerate < 10);  
-SELECT * FROM CarsData ORDER BY Year, ID;
-ALTER TABLE CarsData DROP Edispl, DROP Horsepower, DROP Weight;
-DELETE FROM CarsData WHERE Cylinders <= 5;
-SELECT * FROM CarsData ORDER BY Year, ID;
